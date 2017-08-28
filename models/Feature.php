8507bd85b81dff761e6b9e0c9cd887e06deb8a0e
Kiko Seijo <kiko@sunnyface.com>
2017-08-28 14:01:50 +0200

    Code refactor.

--- models/Feature.php
+++ models/Feature.php
@@ -10,24 +10,42 @@ class Feature extends Model
     use \October\Rain\Database\Traits\Validation;
     use \October\Rain\Database\Traits\Sortable;
 
+    /**
+     * @var mixed
+     */
     public $timestamps = true;
 
+    /**
+     * @var string
+     */
     public $table = 'mavitm_estate_features';
-    public $implement   = ['@RainLab.Translate.Behaviors.TranslatableModel'];
+    /**
+     * @var array
+     */
+    public $implement = ['@RainLab.Translate.Behaviors.TranslatableModel'];
 
+    /**
+     * @var array
+     */
     public $rules = [
-        'title'     => 'required',
-        'slug'      => ['required', 'regex:/^[a-z0-9\/\:_\-\*\[\]\+\?\|]*$/i', 'unique:mavitm_estate_features']
+        'title' => 'required',
+        'slug'  => ['required', 'regex:/^[a-z0-9\/\:_\-\*\[\]\+\?\|]*$/i', 'unique:mavitm_estate_features'],
     ];
 
+    /**
+     * @var array
+     */
     public $belongsToMany = [
         'realty' => [
             'Mavitm\Estate\Models\Realty',
             'table' => 'mavitm_estate_realty_feature',
-            'order' => 'mavitm_estate_realty.updated_at desc'
+            'order' => 'mavitm_estate_realty.updated_at desc',
         ],
-];
+    ];
 
+    /**
+     * @var array
+     */
     public $translatable = ['title'];
 
     public function afterDelete()
@@ -35,17 +53,26 @@ class Feature extends Model
         $this->realty()->detach();
     }
 
+    /**
+     * @param $pageName
+     * @param $controller
+     * @return mixed
+     */
     public function setUrl($pageName, $controller)
     {
         $params = [
-            'id' => $this->id,
+            'id'   => $this->id,
             'slug' => $this->slug,
         ];
 
         return $this->url = $controller->pageUrl($pageName, $params);
     }
 
-    public function getCountRealtyAttribute(){
+    /**
+     * @return mixed
+     */
+    public function getCountRealtyAttribute()
+    {
         return $this->realty->count();
     }
 }
