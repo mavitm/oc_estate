3b5f110c26a9566d25af778c0ba4f1ef9a3d34a5
Ayhan Eraslan <ayer50@gmail.com>
2022-01-20 14:59:03 +0300

    Update builder_table_add_address_to_realty.php

--- updates/builder_table_add_address_to_realty.php
+++ updates/builder_table_add_address_to_realty.php
@@ -3,7 +3,7 @@
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
-class BuilderTableCreateMavitmEstateCategories extends Migration
+class BuilderTableAddAddressToRealty extends Migration
 {
     public function up()
     {
